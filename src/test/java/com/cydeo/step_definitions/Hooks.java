092c4eebf8f75b7d1ed90fd6ddb1602c41c2d49a
w10 <glsmh.16@gmail.com>
2023-03-05 22:12:00 +0300

    Hook

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,7 +1,9 @@
 package com.cydeo.step_definitions;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 
 /*
 In the class we will be able to pass pre- & post- conditions to each scenerio and each step
@@ -10,11 +12,32 @@ In the class we will be able to pass pre- & post- conditions to each scenerio an
 public class Hooks {
     //import from io.cucumber.java not from junit
     @Before
-    public void setup(){
-        System.out.println("Before Scenario is running");
+    public void setupScenerio(){
+        System.out.println("===Setting up browser using cucumber @Before");
+    }
+
+    @Before ("@login")  //only use login anotation
+    public void setupScenerioForLogins(){
+        System.out.println("===this will only apply to scenerios with @login tag");
+    }
+    @Before ("@db")  //only use login anotation
+    public void setupForDataBaseScenerio(){
+        System.out.println("===this will only apply to scenerios with @login tag");
     }
     @After
-    public void teardown(){
-        System.out.println("After Scenario is running");
+    public void teardownScenerio(){
+        System.out.println(" ===Closing browser using cucumber @After");
+        System.out.println("===Scenerio ended/ Take screenshot if failed!");
+    }
+
+    @BeforeStep
+    public void setup(){
+        System.out.println("-----Applying setup using @BeforeStep");
+
+    }
+
+    @AfterStep
+    public void afterStep(){
+        System.out.println("------Applying teardown  using @AfterStep");
     }
 }
