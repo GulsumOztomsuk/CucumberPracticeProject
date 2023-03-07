67edcd6fd6bb8fed2a65abe640ebd100056d88c6
w10 <glsmh.16@gmail.com>
2023-03-07 12:15:45 +0300

    runner

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,  //cuke runner da biseyler yazması için buranın false olması lazım
-        tags = "@Regression"
+        tags = "@scenarioOutline"
 )
 public class CukesRunner {
 
