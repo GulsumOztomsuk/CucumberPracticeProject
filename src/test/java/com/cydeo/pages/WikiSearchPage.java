90499001d9f94bce5dc49d4080ab0c05b8ca25e3
w10 <glsmh.16@gmail.com>
2023-03-07 12:13:48 +0300

    Pages

--- src/test/java/com/cydeo/pages/WikiSearchPage.java
+++ src/test/java/com/cydeo/pages/WikiSearchPage.java
@@ -10,9 +10,14 @@ public class WikiSearchPage {
     public WikiSearchPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
-    @FindBy(id="searchInput")
+
+    @FindBy(id = "searchInput")
     public WebElement searchBox;
 
-    @FindBy(xpath="//button[@class='submit']")
+    @FindBy(xpath = "//button[@type='submit']")
     public WebElement searchButton;
+
+    @FindBy(id = "firstHeading")
+    public WebElement mainHeader;
+
 }
